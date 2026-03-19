90861d0709d54a9c8ef5fd2e078af2a1d2105bee
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-20 05:28:54 +0530

    lazy loading model fix

--- backend/app/model/predictor.py
+++ backend/app/model/predictor.py
@@ -1,14 +1,17 @@
 from transformers import pipeline
 
-class EmotionPredictor:
-    def __init__(self):
-        print("Loading HuggingFace model...")
-        self.classifier=pipeline("text-classification",model="j-hartmann/emotion-english-distilroberta-base",top_k=None,device=-1)
-        print("Model Loaded!")
-    def predict(self,text:str):
-        result=self.classifier(text)[0]
-        return {
-            item["label"].lower():round(item["score"],3)
-            for item in result
-        }
-predictor = EmotionPredictor()
+classifier = None
+
+def get_model():
+    global classifier
+    if classifier is None:
+        classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base",top_k=None)
+    print("Model loaded successfully.")
+    return classifier
+
+def predict_emotion(text: str):
+    model = get_model()
+    results = model(text)[0]
+    
+    emotions={item["label"]:round(item["score"], 3) for item in results}
+    return emotions
--- backend/app/routes/predict.py
+++ backend/app/routes/predict.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from app.model.predictor import predict_emotion
 
 router = APIRouter()
 
+class TextInput(BaseModel):
+    text: str
 
 @router.post("/predict")
-def predict():
-    return {"msg": "This is the predict endpoint!"}
+def predict(data:TextInput):
+    result = predict_emotion(data.text)
+    return result
