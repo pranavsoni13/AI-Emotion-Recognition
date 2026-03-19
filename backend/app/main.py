36fb5548e271f4d7f9e2de0c6774b14017094128
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-20 03:31:55 +0530

    added predict route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routes.predict import router as predict_router
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Backend is Working!"}
+
+app.include_router(predict_router)
