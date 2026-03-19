47a80b8c2c2366f025a240b2e86792f32ddde8dc
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-20 03:12:24 +0530

    test fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.routes.predict import router as predict_router
-from app.routes.analytics import router as analytics_router
-from app.database.database import engine, Base
-from app.database import models
 
-app = FastAPI(
-    title="AI Emotion Recognition API",
-    version="1.0"
-)
+app = FastAPI()
 
-# CORS FIX
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # frontend URL
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-Base.metadata.create_all(bind=engine)
-
-app.include_router(predict_router)
-app.include_router(analytics_router)
+@app.get("/")
+def home():
+    return {"message": "Backend is Working!"}
