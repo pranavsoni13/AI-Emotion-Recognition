a2f6fad5755bd0bd505e59bb66da213ba1289662
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-20 14:43:36 +0530

    final safe model fix

--- backend/app/model/predictor.py
+++ backend/app/model/predictor.py
@@ -1,17 +1,17 @@
-from transformers import pipeline
-
 classifier = None
 
-def get_model():
-    global classifier
-    if classifier is None:
-        classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base",top_k=None)
-    print("Model loaded successfully.")
-    return classifier
-
 def predict_emotion(text: str):
-    model = get_model()
-    results = model(text)[0]
+    global classifier
+    try:
+        if classifier is None:
+            print("Loading model...")
+            from transformers import pipeline
+            classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base",top_k=None)
+            print("Model loaded successfully.")
+        result = classifier(text)[0]
     
-    emotions={item["label"]:round(item["score"], 3) for item in results}
-    return emotions
+        emotions={item["label"]:round(item["score"], 3) for item in result}
+        return emotions
+    except Exception as e:
+        print(f"Error in prediction: {e}")
+        return {"error": str(e)}
