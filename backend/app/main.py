779bc6d42b0ab53e2297f7d4e17b7b69d37f84fb
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-21 15:39:30 +0530

    final model run

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    aloow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
