aa95a1d5789d7b336816cac140cff19a9968e6e9
Pranav Soni <sonipranav2004@gmail.com.com>
2026-03-21 15:03:04 +0530

    final model test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.predict import router as predict_router
+from app.routes.analytics import router as analytics_router
+from app.database.database import engine, Base
+from app.database import models
+import os
 
-app = FastAPI()
+app = FastAPI(
+    title="AI Emotion Recognition API",
+    description="A FastAPI backend for emotion recognition using a pre-trained model.",
+    version="1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    aloow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-def home():
-    return {"message": "Backend is Working!"}
+def root():
+    return {"Status": "API is running"}
 
-app.include_router(predict_router)
+@app.get("/health")
+def health():
+    return {"Status": "Healthy"}
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(predict_router, prefix="/predict", tags=["Predict"])
+app.include_router(analytics_router, prefix="/analytics", tags=["Analytics"])
