90afd429a5268b3eab34c43c4b3d77695a4023a7
Pranav Soni <sonipranav2004@gmail.com.com>
2026-04-09 10:30:47 +0530

    fix api import api

--- frontend/emotion-dashboard/src/components/EmotionInput.jsx
+++ frontend/emotion-dashboard/src/components/EmotionInput.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { predictEmotion } from "../services/api";
+import { API } from "../services/api";
 
 function EmotionInput({ setEmotionData }) {
 
--- frontend/emotion-dashboard/src/services/api.js
+++ frontend/emotion-dashboard/src/services/api.js
@@ -1,7 +1,7 @@
 const API_URL=import.meta.env.VITE_API_URL;
 const BASE_URL=API_URL || "https://ai-emotion-recognition-production.up.railway.app";
 
-export const predictEmotion = async (text) => {
+export const API = async (text) => {
   try {
     const response = await fetch(`${BASE_URL}/predict`, {
       method: "POST",
