c0240fe01555f9ed9a8289b939993f6d9994c4ff
Pranav Soni <sonipranav2004@gmail.com.com>
2026-04-10 22:48:48 +0530

    fix token

--- backend/app/model/predictor.py
+++ backend/app/model/predictor.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
 API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
 
 headers = {
-    "Authorization": "Bearer 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"
+    "Authorization": f"Bearer {os.getenv('HF_TOKEN')}"
 }
 
 def predict_emotion(text: str):
@@ -16,11 +17,9 @@ def predict_emotion(text: str):
 
         data = response.json()
 
-        # 🔴 agar error aaya HF se
         if isinstance(data, dict) and "error" in data:
             return {"error": data["error"]}
 
-        # ✅ safe access
         result = data[0]
 
         emotions = {
@@ -31,5 +30,4 @@ def predict_emotion(text: str):
         return emotions
 
     except Exception as e:
-        print("🔥 ERROR:", e)
         return {"error": str(e)}
