50e191a50f44c4132367463ef13b84a06fd67b9b
Pranav Soni <sonipranav2004@gmail.com.com>
2026-04-10 23:36:05 +0530

    fix api again

--- frontend/emotion-dashboard/src/services/api.js
+++ frontend/emotion-dashboard/src/services/api.js
@@ -10,7 +10,7 @@ const API = {
         headers:{
           "Content-Type":"application/json"
         },
-        body:JSON.stringify({text}),
+        body:JSON.stringify({text:inputText}),
       });
       const data = await response.json();
 
