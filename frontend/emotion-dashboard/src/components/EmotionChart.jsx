bc98ee5f1f486db2926ca66c0978db6746493e45
Pranav Soni <sonipranav2004@gmail.com.com>
2026-04-11 00:15:13 +0530

    fix updated analyze

--- frontend/emotion-dashboard/src/components/EmotionChart.jsx
+++ frontend/emotion-dashboard/src/components/EmotionChart.jsx
@@ -1,4 +1,4 @@
-import { Bar } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   ArcElement,
@@ -30,7 +30,7 @@ function EmotionChart({ data }) {
   };
   return (
     <div style={{height:"300px"}}>
-      <Bar data={chartData} />
+      <Pie data={chartData} />
     </div>
   );
 }
